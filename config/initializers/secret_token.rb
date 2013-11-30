4ebdeb61daadcabbf9f2a48d0cff6fca700a4212
Daniel Lourenco <daniel.antunes.lourenco@gmail.com>
2013-11-30 15:05:11 +0000

    first commit of the social app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialApp::Application.config.secret_token = '266258740d0c96251dd710c3aa69afcf2fb9f089e9d561f67812ae1ac7098bb6c4cb655d95668a0f3cf6220fa5d3b9cb57bce92eddd1bbad3f63ee23c4d7abde'
